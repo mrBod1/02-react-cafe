3e2ba777c8b0ae543cca3d5204f5a5a2e051c1d4
mrBod1 <151578262+mrBod1@users.noreply.github.com>
2026-07-05 23:22:28 +0200

    02-react-cafe

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App/App';
+import App from './components/App/App';
 import 'modern-normalize/modern-normalize.css';
 import './index.css';
 
